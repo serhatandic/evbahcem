c7e6670b2fe4aa4beddbf652e63d6953cbf696ca
serhat andic <serhatandic42@gmail.com>
2023-08-23 15:00:21 +0300

    update grid width for mobile devices

--- app/components/DiseasesGrid.tsx
+++ app/components/DiseasesGrid.tsx
@@ -3,7 +3,7 @@ import sicknesses from '../data/data.json';
 
 const DiseasesGrid = () => {
 	return (
-		<div className='p-24 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4'>
+		<div className='md:p-24 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 p-4'>
 			{Object.values(sicknesses).map((sickness, idx) => (
 				<DiseaseCard key={idx} sickness={sickness} id={idx} />
 			))}
