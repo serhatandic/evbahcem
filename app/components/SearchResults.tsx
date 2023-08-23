ba0175a69df134234295f6a17848d121105d0345
serhat andic <serhatandic42@gmail.com>
2023-08-23 17:00:56 +0300

    update search result linking

--- app/components/SearchResults.tsx
+++ app/components/SearchResults.tsx
@@ -13,8 +13,12 @@ const SearchResults = ({ data }: Props) => {
 	return (
 		<div className='absolute top-16 w-8/12 md:w-6/12 lg:w-5/12'>
 			{data.slice(0, 3)?.map((item: Item) => {
+				const titleWithoutSpaces = item.title.replace(/\s/g, '-');
 				return (
-					<Link key={item.title} href={`hastaliklar/${item.index}`}>
+					<Link
+						key={item.title}
+						href={`hastaliklar/${item.index}/${titleWithoutSpaces}`}
+					>
 						<div className=' bg-white gap-2 border-b-2 p-2'>
 							{item.title}
 						</div>
