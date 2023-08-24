4abdfc7b03e0d5c79c74195e8f8965e5d501fe9f
serhat andic <serhatandic42@gmail.com>
2023-08-24 16:15:01 +0300

    add estimated reading times for articles

--- app/components/ArticleCard.tsx
+++ app/components/ArticleCard.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import readingTime from 'reading-time';
 
 type ArticleCardProps = {
 	id: number;
@@ -13,16 +14,26 @@ type ArticleCardProps = {
 
 const ArticleCard = ({ article, id }: ArticleCardProps) => {
 	const titleWithoutSpaces = article.title.replace(/\s/g, '-');
+	const wholeText = article.sections
+		.map((section) => section.content)
+		.join(' ');
 
 	return (
-		<div className='w-12/12  bg-orange-100 rounded-2xl p-4 flex flex-col'>
+		<div className='w-12/12  bg-orange-100 rounded-2xl p-4 flex flex-col justify-between'>
 			<h3 className='text-2xl font-semibold'>{article.title}</h3>
-			<div className='flex justify-end mt-auto'>
-				<Link href={`makaleler/${id}/${titleWithoutSpaces}`}>
-					<button className='bg-orange-500 text-white rounded-lg px-4 py-2 mt-8'>
-						Detaylar
-					</button>
-				</Link>
+			<div className='flex  mt-8 justify-between items-end'>
+				<p className='opacity-25 text-xs'>
+					{`Tahmini Okuma Süresi
+					${readingTime(wholeText).minutes.toFixed(1)} dk`}
+				</p>
+				<div className='flex justify-end mt-auto'>
+					<Link
+						className='bg-orange-500 text-white rounded-lg px-4 py-2 mt-8'
+						href={`makaleler/${id}/${titleWithoutSpaces}`}
+					>
+						<button>Detaylar</button>
+					</Link>
+				</div>
 			</div>
 		</div>
 	);
