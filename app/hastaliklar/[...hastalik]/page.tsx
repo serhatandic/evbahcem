95970fba22e7db0f5797a08726ab43038ea6b4d0
serhat andic <serhatandic42@gmail.com>
2023-08-24 20:11:14 +0300

    styling update to improve readability of articles and diseases, trim saglikRehberi.json

--- app/hastaliklar/[...hastalik]/page.tsx
+++ app/hastaliklar/[...hastalik]/page.tsx
@@ -20,35 +20,42 @@ const sicknesses = rawSicknesses as {
 const DiseaseDetails = ({ params }: DiseaseDetailsParams) => {
 	const idx = params.hastalik[0];
 	return (
-		<div className='p-12 md:px-36 lg:px-60 xl:px-80'>
-			<h1 className='text-4xl font-bold'>{sicknesses[idx].title}</h1>
-			<p className='mt-2 mb-4'>{sicknesses[idx].entryParagraph}</p>
-			<div className='bg-orange-100 p-4'>
-				<h3 className='text-2xl'>İçindekiler</h3>
-				<div className='mb-4'>
-					<ul>
-						{sicknesses[idx].sections.map((section, idx) => (
-							<li className='pb-2 last:pb-0 first:pt-2' key={idx}>
-								<a href={`#${section.header}`}>
-									{`-> ${section.header}`}
-								</a>
-							</li>
-						))}
-					</ul>
-				</div>
-			</div>
-			<div>
-				{sicknesses[idx].sections.map((section, idx) => (
-					<div key={idx}>
-						<a id={`${section.header}`}></a>
-						<h3 className='text-2xl font-bold'>{section.header}</h3>
-						<div
-							dangerouslySetInnerHTML={{
-								__html: section.content,
-							}}
-						/>
+		<div className='p-8 md:px-36 lg:px-60 xl:px-80'>
+			<div className='bg-orange-100 p-8'>
+				<h1 className='text-4xl font-bold'>{sicknesses[idx].title}</h1>
+				<p className='mt-2 mb-4'>{sicknesses[idx].entryParagraph}</p>
+				<div className='bg-orange-50 p-4 my-4'>
+					<h3 className='text-2xl'>İçindekiler</h3>
+					<div className='mb-4'>
+						<ul>
+							{sicknesses[idx].sections.map((section, idx) => (
+								<li
+									className='pb-2 last:pb-0 first:pt-2'
+									key={idx}
+								>
+									<a href={`#${section.header}`}>
+										{`-> ${section.header}`}
+									</a>
+								</li>
+							))}
+						</ul>
 					</div>
-				))}
+				</div>
+				<div>
+					{sicknesses[idx].sections.map((section, idx) => (
+						<div key={idx}>
+							<a id={`${section.header}`}></a>
+							<h3 className='text-2xl font-bold'>
+								{section.header}
+							</h3>
+							<div
+								dangerouslySetInnerHTML={{
+									__html: section.content,
+								}}
+							/>
+						</div>
+					))}
+				</div>
 			</div>
 		</div>
 	);
