db90436992aa266a6b92a70e1c2eeb8f4ef64555
serhat andic <serhatandic42@gmail.com>
2023-08-24 20:13:17 +0300

    styling updates for articles

--- app/makaleler/[...makale]/page.tsx
+++ app/makaleler/[...makale]/page.tsx
@@ -19,34 +19,41 @@ const articles = rawArticles as {
 const Article = ({ params }: ArticleParams) => {
 	const idx = params.makale[0];
 	return (
-		<div className='p-12 md:px-36 lg:px-60 xl:px-80'>
-			<h1 className='text-4xl font-bold'>{articles[idx].title}</h1>
-			<div className='bg-orange-100 p-4 my-4'>
-				<h3 className='text-2xl'>İçindekiler</h3>
-				<div className='mb-4'>
-					<ul>
-						{articles[idx].sections.map((section, idx) => (
-							<li className='pb-2 last:pb-0 first:pt-2' key={idx}>
-								<a href={`#${section.header}`}>
-									{`-> ${section.header}`}
-								</a>
-							</li>
-						))}
-					</ul>
-				</div>
-			</div>
-			<div>
-				{articles[idx].sections.map((section, idx) => (
-					<div key={idx}>
-						<a id={`${section.header}`}></a>
-						<h3 className='text-2xl font-bold'>{section.header}</h3>
-						<div
-							dangerouslySetInnerHTML={{
-								__html: section.content,
-							}}
-						/>
+		<div className='p-8 md:px-36 lg:px-60 xl:px-80'>
+			<div className='bg-orange-100 p-8'>
+				<h1 className='text-4xl font-bold'>{articles[idx].title}</h1>
+				<div className='bg-orange-50 p-4 my-4'>
+					<h3 className='text-2xl'>İçindekiler</h3>
+					<div className='mb-4'>
+						<ul>
+							{articles[idx].sections.map((section, idx) => (
+								<li
+									className='pb-2 last:pb-0 first:pt-2'
+									key={idx}
+								>
+									<a href={`#${section.header}`}>
+										{`-> ${section.header}`}
+									</a>
+								</li>
+							))}
+						</ul>
 					</div>
-				))}
+				</div>
+				<div>
+					{articles[idx].sections.map((section, idx) => (
+						<div key={idx}>
+							<a id={`${section.header}`}></a>
+							<h3 className='text-2xl font-bold'>
+								{section.header}
+							</h3>
+							<div
+								dangerouslySetInnerHTML={{
+									__html: section.content,
+								}}
+							/>
+						</div>
+					))}
+				</div>
 			</div>
 		</div>
 	);
