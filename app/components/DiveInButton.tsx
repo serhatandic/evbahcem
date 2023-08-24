86d38b12830e37bb548fd53960689379b142db65
serhat andic <serhatandic42@gmail.com>
2023-08-24 14:33:01 +0300

    remove mini text from main screen for small screens

--- app/components/DiveInButton.tsx
+++ app/components/DiveInButton.tsx
@@ -5,7 +5,7 @@ const DiveInButton = () => {
 	return (
 		<div className='flex justify-between gap-4'>
 			<Link
-				className=' h-24 w-6/12 bg-orange-100 rounded-3xl p-2 text-md  mt-16 font-semibold flex flex-col justify-center items-center'
+				className=' h-24 w-6/12 bg-orange-100 rounded-3xl text-md  mt-16 font-semibold flex flex-col justify-start pl-5 pt-2'
 				href='/hastaliklar'
 			>
 				<div className='flex gap-2'>
@@ -14,22 +14,22 @@ const DiveInButton = () => {
 						<ArrowForwardIcon />
 					</div>
 				</div>
-				<p className='text-xs font-light pl-5'>
+				<p className='text-xs font-light hidden lg:block'>
 					Hastalıklar, belirtiler ve tedaviler hakkında detaylı
 					bilgiler. İhtiyacınız olan bilgilere kolayca ulaşın.
 				</p>
 			</Link>
 			<Link
-				className='h-24 w-6/12 bg-orange-100 rounded-3xl p-2 text-md  mt-16 font-semibold flex flex-col justify-center items-center'
+				className='h-24 w-6/12 bg-orange-100 rounded-3xl text-md  mt-16 font-semibold flex flex-col justify-start pl-5 pt-2'
 				href='/makaleler'
 			>
-				<div className='flex gap-2'>
+				<div className='flex gap-2 '>
 					<button className=''>Makalelere Göz At</button>
 					<div className=''>
 						<ArrowForwardIcon />
 					</div>
 				</div>
-				<p className='text-xs font-light pl-5'>
+				<p className='text-xs font-light  hidden lg:block'>
 					Sağlık ve yaşam konularında uzmanlardan makaleler. Bilinçli
 					yaşam kılavuzunuz.
 				</p>
