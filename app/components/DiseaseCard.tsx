c3d0dd8a0127fde8201be0401c3b543fa9a6e870
serhat andic <serhatandic42@gmail.com>
2023-08-24 14:24:39 +0300

    add reading time for sicknesses, update main screen (2 buttons instead of 1)

--- app/components/DiseaseCard.tsx
+++ app/components/DiseaseCard.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import readingTime from 'reading-time';
 
 type DiseaseCardProps = {
 	id: number;
@@ -14,19 +15,29 @@ type DiseaseCardProps = {
 
 const DiseaseCard = ({ sickness, id }: DiseaseCardProps) => {
 	const titleWithoutSpaces = sickness.title.replace(/\s/g, '-');
+	const wholeText =
+		sickness.entryParagraph +
+		sickness.sections.map((section) => section.content).join(' ');
 	return (
-		<div className='w-12/12  bg-orange-100 rounded-2xl p-4 flex flex-col'>
-			<h3 className='text-2xl font-semibold'>{sickness.title}</h3>
-			<p className='mt-4'>
-				{sickness.entryParagraph?.slice(0, 250) + '...'}
-			</p>
-
-			<div className='flex justify-end mt-auto'>
-				<Link href={`hastaliklar/${id}/${titleWithoutSpaces}`}>
-					<button className='bg-orange-500 text-white rounded-lg px-4 py-2 mt-8'>
-						Daha Fazla
-					</button>
-				</Link>
+		<div className='w-12/12  bg-orange-100 rounded-2xl p-4 flex flex-col justify-between'>
+			<div>
+				<h3 className='text-2xl font-semibold'>{sickness.title}</h3>
+				<p className='mt-4'>
+					{sickness.entryParagraph?.slice(0, 250) + '...'}
+				</p>
+			</div>
+			<div className='flex  mt-8 justify-between items-end'>
+				<p className='opacity-25 text-xs'>
+					{`Tahmini Okuma Süresi
+					${readingTime(wholeText).minutes.toFixed(0)} dk`}
+				</p>
+				<div className='flex justify-end mt-auto'>
+					<Link href={`hastaliklar/${id}/${titleWithoutSpaces}`}>
+						<button className='bg-orange-500 text-white rounded-lg px-4 py-2'>
+							Daha Fazla
+						</button>
+					</Link>
+				</div>
 			</div>
 		</div>
 	);
--- app/components/DiveInButton.tsx
+++ app/components/DiveInButton.tsx
@@ -1,12 +1,40 @@
 import Link from 'next/link';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const DiveInButton = () => {
 	return (
-		<Link href='/hastaliklar'>
-			<div className='h-12 w-full bg-black rounded-full p-2 text-md  mt-16 font-semibold text-white flex flex-col justify-center items-center'>
-				<button className=''>Keşfet</button>
-			</div>{' '}
-		</Link>
+		<div className='flex justify-between gap-4'>
+			<Link
+				className=' h-24 w-6/12 bg-orange-100 rounded-3xl p-2 text-md  mt-16 font-semibold flex flex-col justify-center items-center'
+				href='/hastaliklar'
+			>
+				<div className='flex gap-2'>
+					<button className=''>Hastalıkları Keşfet</button>
+					<div className=''>
+						<ArrowForwardIcon />
+					</div>
+				</div>
+				<p className='text-xs font-light pl-5'>
+					Hastalıklar, belirtiler ve tedaviler hakkında detaylı
+					bilgiler. İhtiyacınız olan bilgilere kolayca ulaşın.
+				</p>
+			</Link>
+			<Link
+				className='h-24 w-6/12 bg-orange-100 rounded-3xl p-2 text-md  mt-16 font-semibold flex flex-col justify-center items-center'
+				href='/makaleler'
+			>
+				<div className='flex gap-2'>
+					<button className=''>Makalelere Göz At</button>
+					<div className=''>
+						<ArrowForwardIcon />
+					</div>
+				</div>
+				<p className='text-xs font-light pl-5'>
+					Sağlık ve yaşam konularında uzmanlardan makaleler. Bilinçli
+					yaşam kılavuzunuz.
+				</p>
+			</Link>
+		</div>
 	);
 };
 
