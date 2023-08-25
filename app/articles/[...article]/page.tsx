6b6c50d3de3a8148f13bddfce88574ea3e40b280
serhat andic <serhatandic42@gmail.com>
2023-08-25 21:02:16 +0300

    text contrast improvement

--- app/articles/[...article]/page.tsx
+++ app/articles/[...article]/page.tsx
@@ -105,7 +105,7 @@ const Article = ({ params }: ArticleParams) => {
 				</div>
 				<div className='flex justify-between pt-3 gap-3 flex-col sm:flex-row'>
 					<Link
-						className='bg-orange-800 flex-1 text-white rounded-lg px-4 py-2 flex justify-center'
+						className='bg-orange-500 flex-1 text-white rounded-lg px-4 py-2 flex justify-center'
 						href={'/diseases'}
 					>
 						<button className='flex justify-center text-center'>
@@ -113,13 +113,13 @@ const Article = ({ params }: ArticleParams) => {
 						</button>
 					</Link>
 					<Link
-						className='bg-orange-800 flex-1 text-white rounded-lg px-4 py-2 flex justify-center'
+						className='bg-orange-500 flex-1 text-white rounded-lg px-4 py-2 flex justify-center'
 						href={`/`}
 					>
 						<button>Home</button>
 					</Link>
 					<Link
-						className='bg-orange-800 flex-1 text-white rounded-lg px-4 py-2 flex justify-center'
+						className='bg-orange-500 flex-1 text-white rounded-lg px-4 py-2 flex justify-center'
 						href={`/articles`}
 					>
 						<button>Check Other Articles</button>
