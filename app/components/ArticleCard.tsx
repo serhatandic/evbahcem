d67c081a467c3e4be9e377819579b5901987b1be
serhat andic <serhatandic42@gmail.com>
2023-08-29 01:28:21 +0300

    re-color details button

--- app/components/ArticleCard.tsx
+++ app/components/ArticleCard.tsx
@@ -31,7 +31,7 @@ const ArticleCard = ({ article, id }: ArticleCardProps) => {
 				</p>
 				<div className='flex justify-end mt-auto'>
 					<Link
-						className='bg-orange-500 text-white rounded-lg px-4 py-2 mt-2'
+						className='bg-gray-800 text-white rounded-lg px-4 py-2 mt-2'
 						href={`articles/${id}/${titleWithoutSpaces}`}
 					>
 						<button>Details</button>
