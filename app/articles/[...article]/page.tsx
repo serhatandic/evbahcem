d2723b4e3ae411ff4411c31dc5eecdf3b1420b0f
serhat andic <serhatandic42@gmail.com>
2023-08-30 14:30:13 +0300

    writer info section styling

--- app/articles/[...article]/page.tsx
+++ app/articles/[...article]/page.tsx
@@ -45,32 +45,35 @@ const Article = ({ params }: ArticleParams) => {
 		<>
 			<div className='p-8 md:px-36 lg:px-60 xl:px-80'>
 				<div className='bg-orange-100 p-8'>
-					<div className='flex gap-2 items-center mb-2'>
-						<p className='flex items-center gap-2'>
-							<Image
-								className='rounded-full'
-								src={'/author.png'}
-								alt='author'
-								width={40}
-								height={40}
-							/>
-							<span className='text-gray-800 opacity-80'>
-								Written by camping expert{' '}
-								<Link href={'/about'}>
-									<strong>Archibald Whitney</strong>
-								</Link>
-							</span>
-						</p>
-						<span>/</span>
-						<p className='opacity-80 text-gray-800 font-normal'>
-							{
-								readingTime(
-									articles[idx].sections
-										.map((section) => section.content)
-										.join(' ')
-								).text
-							}
-						</p>
+					<div className='flex gap-2 items-center md:items-center mb-2'>
+						<Image
+							className='rounded-full'
+							src={'/author.png'}
+							alt='author'
+							width={50}
+							height={50}
+						/>
+						<div className='flex flex-col gap-2'>
+							<p className='flex items-start flex-col'>
+								<span className='text-gray-800 opacity-80 text-sm md:text-base lg:text-lg'>
+									Written by camping expert{' '}
+									<Link href={'/about'}>
+										<strong>Archibald Whitney</strong>
+									</Link>
+								</span>
+								<p className='opacity-80 text-gray-800 font-normal'>
+									{
+										readingTime(
+											articles[idx].sections
+												.map(
+													(section) => section.content
+												)
+												.join(' ')
+										).text
+									}
+								</p>
+							</p>
+						</div>
 					</div>
 					<h1 className='text-4xl font-bold mb-2'>
 						{articles[idx].title}
