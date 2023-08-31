96d1e69413754f9c20bc173128471efb90eac667
serhat andic <serhatandic42@gmail.com>
2023-08-31 13:06:34 +0300

    update sitemap

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -27,111 +27,111 @@ export default function sitemap(): MetadataRoute.Sitemap {
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/26/Unveiling-the-Best-Camping-Locations-Near-Waterbury,-Connecticut',
+			url: 'https://www.campingatwild.com/articles/26/unveiling-the-best-camping-locations-near-waterbury-connecticut',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/25/Discover-The-Best-Camping-Locations-Near-Henderson,-Nevada',
+			url: 'https://www.campingatwild.com/articles/25/discover-the-best-camping-locations-near-henderson-nevada',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/24/Top-10-Tents-to-Make-Your-Camping-Trip-Wildly-Comfortable',
+			url: 'https://www.campingatwild.com/articles/24/top-10-tents-to-make-your-camping-trip-wildly-comfortable',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/23/Camping-on-a-Budget:-How-to-Save-Money-on-Your-Next-Trip',
+			url: 'https://www.campingatwild.com/articles/23/camping-on-a-budget-how-to-save-money-on-your-next-trip',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/22/Fishing-and-Camping:-The-Perfect-Outdoor-Combo',
+			url: 'https://www.campingatwild.com/articles/22/fishing-and-camping-the-perfect-outdoor-combo',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/21/Survival-Skills:-Basic-Techniques-to-Know-When-Camping',
+			url: 'https://www.campingatwild.com/articles/21/survival-skills-basic-techniques-to-know-when-camping',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/20/Camping-vs.-Backpacking:-What%E2%80%99s-the-Difference?',
+			url: 'https://www.campingatwild.com/articles/20/camping-vs-backpacking-what-s-the-difference',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/19/The-History-of-Camping:-A-Journey-Through-Time',
+			url: 'https://www.campingatwild.com/articles/19/the-history-of-camping-a-journey-through-time',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/18/The-Ultimate-Guide-to-Essential-Knots-Every-Camper-Should-Know',
+			url: 'https://www.campingatwild.com/articles/18/the-ultimate-guide-to-essential-knots-every-camper-should-know',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/17/5-Luxury-Camping-Spots-Perfect-for-Glampers:-Experience-the-Great-Outdoors-in-Unmatched-Comfort',
+			url: 'https://www.campingatwild.com/articles/17/5-luxury-camping-spots-perfect-for-glampers-experience-the-great-outdoors-in-unmatched-comfort',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/16/Stargazing-While-Camping:-A-Beginner%E2%80%99s-Guide-to-the-Night-Sky',
+			url: 'https://www.campingatwild.com/articles/16/stargazing-while-camping-a-beginner-s-guide-to-the-night-sky',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/15/The-Best-Seasons-for-Camping:-A-Month-by-Month-Guide',
+			url: 'https://www.campingatwild.com/articles/15/the-best-seasons-for-camping-a-month-by-month-guide',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/14/Creative-Camping-Recipes:-Cooking-Without-a-Kitchen',
+			url: 'https://www.campingatwild.com/articles/14/creative-camping-recipes-cooking-without-a-kitchen',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/13/Solo-Camping:-A-Guide-to-Finding-Solitude-in-Nature',
+			url: 'https://www.campingatwild.com/articles/13/solo-camping-a-guide-to-finding-solitude-in-nature',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/12/Camping-with-Kids:-Tips-and-Tricks-for-a-Stress-Free-Trip',
+			url: 'https://www.campingatwild.com/articles/12/camping-with-kids-tips-and-tricks-for-a-stress-free-trip',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/11/The-Incredible-Health-Benefits-of-Spending-Time-in-Nature-While-Camping:-A-Guide-for--Explorers',
+			url: 'https://www.campingatwild.com/articles/11/the-incredible-health-benefits-of-spending-time-in-nature-while-camping-a-guide-for-explorers',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/10/Eco-Friendly-Camping:-How-to-Minimize-Your-Footprint',
+			url: 'https://www.campingatwild.com/articles/10/eco-friendly-camping-how-to-minimize-your-footprint',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/9/The-Top-5-Camping-Destinations-for-Families:-Where-Adventure-Meets-Fun',
+			url: 'https://www.campingatwild.com/articles/9/the-top-5-camping-destinations-for-families-where-adventure-meets-fun',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: "https://campingatwild.com/articles/8/Beginner's-Guide:-How-to-Build-a-Campfire-Safely",
+			url: 'https://www.campingatwild.com/articles/8/beginner-s-guide-how-to-build-a-campfire-safely',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/7/How-to-Choose-the-Best-Camping-Tent-for-Your-Outdoor-Excursions',
+			url: 'https://www.campingatwild.com/articles/7/how-to-choose-the-best-camping-tent-for-your-outdoor-excursions',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/6/10-Must-Have-Gadgets-for-Your-Next-Camping-Adventure',
+			url: 'https://www.campingatwild.com/articles/6/10-must-have-gadgets-for-your-next-camping-adventure',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/5/The-Comprehensive-Guide-to-Camping-Essentials:-A-Detailed-Checklist',
+			url: 'https://www.campingatwild.com/articles/5/the-comprehensive-guide-to-camping-essentials-a-detailed-checklist',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/4/The-Ultimate-Camping-Checklist:-Everything-You-Need-for-an-Amazing-Adventure',
+			url: 'https://www.campingatwild.com/articles/4/the-ultimate-camping-checklist-everything-you-need-for-an-amazing-adventure',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/3/Essentials-To-Take-With-You-When-Hitting-The-Road',
+			url: 'https://www.campingatwild.com/articles/3/essentials-to-take-with-you-when-hitting-the-road',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/2/5-Life-Saving-Tips-for-Camping',
+			url: 'https://www.campingatwild.com/articles/2/5-life-saving-tips-for-camping',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/1/How-to-Stay-Warm-In-a-Sleeping-Bag',
+			url: 'https://www.campingatwild.com/articles/1/how-to-stay-warm-in-a-sleeping-bag',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 		{
-			url: 'https://campingatwild.com/articles/0/12-Important-Points-to-Consider-When-Camping-in-Hot-Weather',
+			url: 'https://www.campingatwild.com/articles/0/12-important-points-to-consider-when-camping-in-hot-weather',
 			lastModified: '2023-08-30T13:52:26+00:00',
 		},
 	];
