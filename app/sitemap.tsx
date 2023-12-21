bb40a35682d3ecf6efea381084c26559ac7c5b37
serhat andic <serhatandic42@gmail.com>
2023-12-21 22:23:48 +0300

    update sitemap

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -3,140 +3,44 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
 	return [
 		{
-			url: 'https://campingatwild.com/',
-			lastModified: '2023-09-04',
+			url: 'https://evbahcem.com/',
+			lastModified: '2023-11-21',
 		},
 		{
-			url: 'https://campingatwild.com/articles',
-			lastModified: '2023-09-04',
+			url: 'https://evbahcem.com/articles',
+			lastModified: '2023-11-21',
 		},
 		{
-			url: 'https://campingatwild.com/privacy-policy',
-			lastModified: '2023-09-04',
+			url: 'https://evbahcem.com/privacy-policy',
+			lastModified: '2023-11-21',
 		},
 		{
-			url: 'https://campingatwild.com/terms-of-service',
-			lastModified: '2023-09-04',
+			url: 'https://evbahcem.com/terms-of-service',
+			lastModified: '2023-11-21',
 		},
 		{
-			url: 'https://campingatwild.com/contact',
-			lastModified: '2023-09-04',
+			url: 'https://evbahcem.com/contact',
+			lastModified: '2023-11-21',
 		},
 		{
-			url: 'https://campingatwild.com/about',
-			lastModified: '2023-09-04',
+			url: 'https://evbahcem.com/about',
+			lastModified: '2023-11-21',
 		},
 		{
-			url: 'https://www.campingatwild.com/articles/27/scary-and-funny-campfire-stories-a-guide-to-tales-for-adults-and-kids',
-			lastModified: '2023-09-08',
+			url: 'https://www.evbahcem.com/articles/0/kucuk-alanlarda-verimli-bahcecilik-balkonlar-ve-kucuk-bahceler-icin-bitki-onerileri-ve-duzenleme-ipuclari',
+			lastModified: '2023-11-21',
 		},
 		{
-			url: 'https://www.campingatwild.com/articles/26/unveiling-the-best-camping-locations-near-waterbury-connecticut',
-			lastModified: '2023-09-04',
+			url: 'https://www.evbahcem.com/articles/1/evde-mikro-yesillikler-yetistirmek',
+			lastModified: '2023-11-21',
 		},
 		{
-			url: 'https://www.campingatwild.com/articles/25/discover-the-best-camping-locations-near-henderson-nevada',
-			lastModified: '2023-09-04',
+			url: 'https://www.evbahcem.com/articles/2/surdurulebilir-mikro-bahcecilik-cevreye-duyarli-bahcecilik-yontemleri-ve-organik-tarim-uygulamalari',
+			lastModified: '2023-11-21',
 		},
 		{
-			url: 'https://www.campingatwild.com/articles/24/top-10-tents-to-make-your-camping-trip-wildly-comfortable',
-			lastModified: '2023-09-04',
-		},
-		{
-			url: 'https://www.campingatwild.com/articles/23/camping-on-a-budget-how-to-save-money-on-your-next-trip',
-			lastModified: '2023-09-04',
-		},
-		{
-			url: 'https://www.campingatwild.com/articles/22/fishing-and-camping-the-perfect-outdoor-combo',
-			lastModified: '2023-09-04',
-		},
-		{
-			url: 'https://www.campingatwild.com/articles/21/survival-skills-basic-techniques-to-know-when-camping',
-			lastModified: '2023-09-04',
-		},
-		{
-			url: 'https://www.campingatwild.com/articles/20/camping-vs-backpacking-what-s-the-difference',
-			lastModified: '2023-09-04',
-		},
-		{
-			url: 'https://www.campingatwild.com/articles/19/the-history-of-camping-a-journey-through-time',
-			lastModified: '2023-09-04',
-		},
-		{
-			url: 'https://www.campingatwild.com/articles/18/the-ultimate-guide-to-essential-knots-every-camper-should-know',
-			lastModified: '2023-09-04',
-		},
-		{
-			url: 'https://www.campingatwild.com/articles/17/5-luxury-camping-spots-perfect-for-glampers-experience-the-great-outdoors-in-unmatched-comfort',
-			lastModified: '2023-09-04',
-		},
-		{
-			url: 'https://www.campingatwild.com/articles/16/stargazing-while-camping-a-beginner-s-guide-to-the-night-sky',
-			lastModified: '2023-09-04',
-		},
-		{
-			url: 'https://www.campingatwild.com/articles/15/the-best-seasons-for-camping-a-month-by-month-guide',
-			lastModified: '2023-09-04',
-		},
-		{
-			url: 'https://www.campingatwild.com/articles/14/creative-camping-recipes-cooking-without-a-kitchen',
-			lastModified: '2023-09-04',
-		},
-		{
-			url: 'https://www.campingatwild.com/articles/13/solo-camping-a-guide-to-finding-solitude-in-nature',
-			lastModified: '2023-09-04',
-		},
-		{
-			url: 'https://www.campingatwild.com/articles/12/camping-with-kids-tips-and-tricks-for-a-stress-free-trip',
-			lastModified: '2023-09-04',
-		},
-		{
-			url: 'https://www.campingatwild.com/articles/11/the-incredible-health-benefits-of-spending-time-in-nature-while-camping-a-guide-for-explorers',
-			lastModified: '2023-09-04',
-		},
-		{
-			url: 'https://www.campingatwild.com/articles/10/eco-friendly-camping-how-to-minimize-your-footprint',
-			lastModified: '2023-09-04',
-		},
-		{
-			url: 'https://www.campingatwild.com/articles/9/the-top-5-camping-destinations-for-families-where-adventure-meets-fun',
-			lastModified: '2023-09-04',
-		},
-		{
-			url: 'https://www.campingatwild.com/articles/8/beginner-s-guide-how-to-build-a-campfire-safely',
-			lastModified: '2023-09-04',
-		},
-		{
-			url: 'https://www.campingatwild.com/articles/7/how-to-choose-the-best-camping-tent-for-your-outdoor-excursions',
-			lastModified: '2023-09-04',
-		},
-		{
-			url: 'https://www.campingatwild.com/articles/6/10-must-have-gadgets-for-your-next-camping-adventure',
-			lastModified: '2023-09-04',
-		},
-		{
-			url: 'https://www.campingatwild.com/articles/5/the-comprehensive-guide-to-camping-essentials-a-detailed-checklist',
-			lastModified: '2023-09-04',
-		},
-		{
-			url: 'https://www.campingatwild.com/articles/4/the-ultimate-camping-checklist-everything-you-need-for-an-amazing-adventure',
-			lastModified: '2023-09-04',
-		},
-		{
-			url: 'https://www.campingatwild.com/articles/3/essentials-to-take-with-you-when-hitting-the-road',
-			lastModified: '2023-09-04',
-		},
-		{
-			url: 'https://www.campingatwild.com/articles/2/5-life-saving-tips-for-camping',
-			lastModified: '2023-09-04',
-		},
-		{
-			url: 'https://www.campingatwild.com/articles/1/how-to-stay-warm-in-a-sleeping-bag',
-			lastModified: '2023-09-04',
-		},
-		{
-			url: 'https://www.campingatwild.com/articles/0/12-important-points-to-consider-when-camping-in-hot-weather',
-			lastModified: '2023-09-04',
+			url: 'https://www.evbahcem.com/articles/3/cocuklarla-mikro-bahcecilik-eglenceli-ve-egitici-projeler',
+			lastModified: '2023-11-21',
 		},
 	];
 }
