2acb51f5126da8791e952da478c6b7df97940a83
serhat andic <serhatandic42@gmail.com>
2023-12-22 16:12:57 +0300

    add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google';
 import { Providers } from './components/Providers';
 import Footer from './components/Footer';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
